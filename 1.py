2ea334f348ee894a7ae3d4bf5e58692e545e72f3
erkitammeaid <erkipoiss01@gmail.com>
2020-12-01 09:31:58 +0200

    sss

---
+++ 1.py
@@ -0,0 +1,9 @@
+# avame input faili lugemiseks
+# loe faili read listi (readlines())
+# kordus yle faili ridade ja loeme rea
+    # rida numbriks
+    # vastus = 2020 - number
+    # kontrollime, kas vastus on listis (andmetyybid: vastus on number ja failis)
+        # v2ljastame number * vastus
+
+f = open('input.txt', 'r')
